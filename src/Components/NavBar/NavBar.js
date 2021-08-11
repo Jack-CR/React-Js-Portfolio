076b31079df23a7aa1d75753d927581fbe65c73a
Yacdany <jack20-21@hotmail.com>
2021-08-11 17:04:54 -0600

    Adding social media icons to the navBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,27 +1,36 @@
 import React from 'react'
-import {Nav} from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import {
+ImWhatsapp,
+AiFillGithub,
+AiFillLinkedin
+
+} from 'react-icons/all'
 
 export const NavBar = () => {
     return (
         <>
-            <Nav className="justify-content-center" activeKey="/home">
+            <Nav variant="tabs" className="justify-content-center" activeKey="/home">
                 <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
+                    <Nav.Link href="/home">Lenguage</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="disabled" disabled>
-                        Disabled
-                    </Nav.Link>
+                    <Nav.Link href="/home">Tema</Nav.Link>
                 </Nav.Item>
             </Nav>
-            <p className="text-center mt-4 mb-4">Or right-aligned</p>
-            <Nav className="justify-content-end" activeKey="/home">
+            <Nav variant="tabs" className="justify-content-center" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home"><ImWhatsapp size={30} color="green"/></Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1"><AiFillGithub size={30} color="black"/></Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2"><AiFillLinkedin size={30} color="blue"/></Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <p className="text-center mt-4 mb-4">Button About me</p>
+            <Nav variant="tabs" className="justify-content-start" activeKey="/home">
                 <Nav.Item>
                     <Nav.Link href="/home">Active</Nav.Link>
                 </Nav.Item>
