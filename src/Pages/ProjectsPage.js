7c578d0c3cfb41bb0168140ca7c5e6037ea9c7df
Yacdany <jack20-21@hotmail.com>
2021-08-12 10:22:06 -0600

    Creating card component

--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import CardsComponent from '../Components/Cards/CardsComponent'
+
+const MyProjects = [
+    {
+        name: "Shopping Cart",
+        tecnologies: {
+            tec_1: "Laravel",
+            tec_2: "Mysql"
+        }
+    },
+    {
+        name: "venta",
+        tecnologies: {
+            tec_1: "Mysql",
+            tec_2: "java"
+        }
+    }
+]
 
 export const ProjectsPage = () => {
     return (
-        <div>
-            <h1>Projects Page</h1>
-        </div>
+        <Container>
+            <Row>
+                {MyProjects.map((el, key) => {
+                    return (
+                        <Col>
+                            <CardsComponent project={el} key={key} />
+                        </Col>
+                    )
+                })}
+            </Row>
+        </Container>
     )
 }
 
