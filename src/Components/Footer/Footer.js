40d7cdd50e718decb2a9de0ea68296cea56b8495
Yacdany <jack20-21@hotmail.com>
2021-08-11 20:41:01 -0600

    new phrase to the modal

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => {
+    const date=new Date().getFullYear();
+
+    return (
+        <label>Yacdany Arguedas Mora ©{date}</label>
+    )
+}
+
+export default Footer
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -28,7 +28,7 @@ const language = {
         navMenu_1: "Skills",
         navMenu_2: "Projects",
         navMenu_3: "Resume",
-        modalAbout: "!Know me¡",
+        modalAbout: "!Meet Me¡",
         modalHeader: "¡Hi I'm junior web Developer!"
     },
     es: {
@@ -63,59 +63,65 @@ export const NavBar = () => {
     return (
         <Router>
             <div class={theme}>
-            <Nav variant="tabs" className="justify-content-center" activeKey="/home">
-                <Nav.Item>
-                    <Form.Select aria-label="Default select example" onChange={handleLanguage}>
-                        <option value="en">EN</option>
-                        <option value="es">ES</option>
-                    </Form.Select>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link onClick={handleTheme}>
-                        {theme === "light"
-                            ? <FaMoon size={30} color="black" />
-                            : <FiSun size={30} color="yellow" />
-                        }
-                    </Nav.Link>
-                </Nav.Item>
-            </Nav>
-            <Nav variant="tabs" className="justify-content-center" activeKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home"><ImWhatsapp size={30} color="green" /></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1"><AiFillGithub size={30} color="black" /></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2"><AiFillLinkedin size={30} color="blue" /></Nav.Link>
-                </Nav.Item>
-            </Nav>
-            <p className="text-center mt-4 mb-4">
-                <ModalJs lang={lang} language={language} />
-            </p>
-            <Nav variant="tabs" className="justify-content-start" activeKey="/">
-                <Nav.Item>
-                    <Nav.Link eventkey="/"><FiCpu size={30} color="gray" /><Link to="/"> {language[lang].navMenu_1}</Link></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1"><MdBusinessCenter color="black" size={30} /><Link to="/Projects">{language[lang].navMenu_2}</Link></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2"><FaFilePdf size={30} color="red" /><Link to="/Resume">{language[lang].navMenu_3}</Link></Nav.Link>
-                </Nav.Item>
-            </Nav>
-                
-            <Switch>
-                <Route path="/Resume">
-                    <ResumePage/>
-                </Route>
-                <Route path="/Projects">
-                    <ProjectsPage/>
-                </Route>
-                <Route path="/">
-                    <SkillsPage/>
-                </Route>
-            </Switch>
+                <Nav  className="justify-content-center" activeKey="/home">
+                    <Nav.Item>
+                        <Form.Select aria-label="Default select example" onChange={handleLanguage}>
+                            <option value="en">EN</option>
+                            <option value="es">ES</option>
+                        </Form.Select>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link onClick={handleTheme}>
+                            {theme === "light"
+                                ? <FaMoon size={30} color="black" />
+                                : <FiSun size={30} color="yellow" />
+                            }
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Nav variant="tabs" className="justify-content-center" defaultActiveKey="/home">
+                    <Nav.Item>
+                        <Nav.Link href="https://api.whatsapp.com/send?phone=50689498924" target="_blank"><ImWhatsapp size={30} color="green" /></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1" href="https://github.com/Jack-CR?tab=repositories" target="_blank">
+                            {theme === "light"
+                                ? <AiFillGithub size={30} color="black" />
+                                : <AiFillGithub size={30} color="white" />
+                            }
+
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#" eventKey="link-2"><AiFillLinkedin size={30} color="blue" /></Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <p className="text-center mt-4 mb-4">
+                    <ModalJs lang={lang} language={language} />
+                </p>
+                <Nav variant="tabs" className="justify-content-start" defaultActiveKey="/home">
+                    <Nav.Item>
+                        <Nav.Link href="/home"><FiCpu size={30} color="gray" /><Link to="/"> {language[lang].navMenu_1}</Link></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1"><MdBusinessCenter color="black" size={30} /><Link to="/Projects">{language[lang].navMenu_2}</Link></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2"><FaFilePdf size={30} color="red" /><Link to="/Resume">{language[lang].navMenu_3}</Link></Nav.Link>
+                    </Nav.Item>
+                </Nav>
+
+                <Switch>
+                    <Route path="/Resume">
+                        <ResumePage />
+                    </Route>
+                    <Route path="/Projects">
+                        <ProjectsPage />
+                    </Route>
+                    <Route path="/">
+                        <SkillsPage />
+                    </Route>
+                </Switch>
             </div>
         </Router>
     )
