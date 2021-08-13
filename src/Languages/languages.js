ab2a6af395e2203c907258e69c41a2c97940daaf
Yacdany <jack20-21@hotmail.com>
2021-08-13 12:22:07 -0600

    Creating a module for the languages

---
+++ src/Languages/languages.js
@@ -0,0 +1,25 @@
+/* ENGLISH AND SPANISH */
+const language = {
+    en: {
+        navMenu_1: "Skills",
+        navMenu_2: "Projects",
+        navMenu_3: "Resume",
+        modalAbout: "!Meet Me¡",
+        modalHeader: "¡Hi I'm Yacdany and I'm a web Developer!",
+        modalAboutMe:"About Me",
+        modalmeDescription:"I am a person who is passionate about technology, who likes challenges and does not give up easily. Very self-taught, I never stop learning.",
+        modalTecIwhant:"Technologies that I would like to learn"
+    },
+    es: {
+        navMenu_1: "Habilidades",
+        navMenu_2: "Proyectos",
+        navMenu_3: "Currículum",
+        modalAbout: "!Conoceme¡",
+        modalHeader: "¡Hola Soy Yacdany y soy Desarrollador Web!",
+        modalAboutMe:"Sobre mi",
+        modalmeDescription:"Soy una persona que le apasiona la tecnologia, que le gusta los retos y no se rinde fácilmente. Muy autodidacta , nunca dejo de aprender.",
+        modalTecIwhant:"Tecnologías que me gustaría aprender"
+    }
+}
+
+export {language}
