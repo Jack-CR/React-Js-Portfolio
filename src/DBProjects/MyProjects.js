16bcc12ab9d9d3a13291424c989b05f4c982c301
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-10-30 20:24:36 -0600

    Adding a new Project

--- src/DBProjects/MyProjects.js
+++ src/DBProjects/MyProjects.js
@@ -252,6 +252,34 @@ const MyProjects = [
             "img/project_8/3.png"
         ]
     },
+    {
+        en: {
+            projectTitle:"Calculator React Js",
+            projectInfo: "Basic 4 calculator operations.",
+            projectBtnInfo: "More Information",
+            canvaBuiltwith:"Built with:"
+        },
+        es: {
+            projectTitle: "Calculadora React Js",
+            projectInfo: "4 operaciones basicas.",
+            projectBtnInfo: "Más Información",
+            canvaBuiltwith:"Construido con:"
+        },
+        imgStart: "img/project_9/1.png",
+        src:"https://dynamic-stroopwafel-062f6f.netlify.app/",
+        tecnologies:
+        [
+            <FaReact size={60} color={reactJs}/>,
+            <DiCss3 size={60} color={css}/>,
+            <IoLogoJavascript size={60} color={js}/>
+        ],
+        imgProject:
+        [
+            "img/project_9/1.png",
+            "img/project_9/2.png",
+            "img/project_9/3.png"
+        ]
+    },
 ]
 
 export {MyProjects}
