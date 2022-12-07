59c62df7351f5e5d2f116252e891a3f06b420507
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-07 13:33:01 -0600

    Currency converter

--- src/DBProjects/MyProjects.js
+++ src/DBProjects/MyProjects.js
@@ -1,13 +1,13 @@
 import {
-AiOutlineHtml5,
-DiCss3,
-FaReact,
-FaLaravel,
-IoLogoJavascript,
-SiMysql,
-FaPhp,
-VscJson,
-DiBootstrap
+    AiOutlineHtml5,
+    DiCss3,
+    FaReact,
+    FaLaravel,
+    IoLogoJavascript,
+    SiMysql,
+    FaPhp,
+    VscJson,
+    DiBootstrap
 } from 'react-icons/all'
 
 const reactJs = "#61DBFB", html = "#f06529", css = "#264de4", js = "#f0db4f", mysql = "#F29111", laravel = "#fb503b", botstrao = "#7952b3";
@@ -301,6 +301,34 @@ const MyProjects = [
             canvaBuiltwith: "Construido con:"
         },
         imgStart: "img/project_9/1.png",
+        src: "https://magnificent-entremet-5bd169.netlify.app/",
+        tecnologies:
+            [
+                <FaReact size={60} color={reactJs} />,
+                <DiCss3 size={60} color={css} />,
+                <IoLogoJavascript size={60} color={js} />
+            ],
+        imgProject:
+            [
+                "img/project_11/1.png",
+                "img/project_11/2.png",
+                "img/project_11/3.png"
+            ]
+    },
+    {
+        en: {
+            projectTitle: "Currency Converter",
+            projectInfo: "Currency converter to different types of currency.",
+            projectBtnInfo: "More Information",
+            canvaBuiltwith: "Built with:"
+        },
+        es: {
+            projectTitle: "Conversor de Moneda",
+            projectInfo: "Conversor de moneda a diferentes tipos de moneda.",
+            projectBtnInfo: "Más Información",
+            canvaBuiltwith: "Construido con:"
+        },
+        imgStart: "img/project_11/1.png",
         src: "https://dynamic-stroopwafel-062f6f.netlify.app/",
         tecnologies:
             [
@@ -310,9 +338,9 @@ const MyProjects = [
             ],
         imgProject:
             [
-                "img/project_9/1.png",
-                "img/project_9/2.png",
-                "img/project_9/3.png"
+                "img/project_11/1.png",
+                "img/project_11/2.png",
+                "img/project_11/3.png"
             ]
     },
 ]
