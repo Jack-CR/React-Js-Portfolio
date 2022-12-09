d5a31c23dfe46f1b3872778bb6ac89cfad04d6cd
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-09 16:21:42 -0600

    New project

--- src/DBProjects/MyProjects.js
+++ src/DBProjects/MyProjects.js
@@ -7,10 +7,11 @@ import {
     SiMysql,
     FaPhp,
     VscJson,
-    DiBootstrap
+    DiBootstrap,
+    FaNodeJs
 } from 'react-icons/all'
 
-const reactJs = "#61DBFB", html = "#f06529", css = "#264de4", js = "#f0db4f", mysql = "#F29111", laravel = "#fb503b", botstrao = "#7952b3";
+const reactJs = "#61DBFB", html = "#f06529", css = "#264de4", js = "#f0db4f", mysql = "#F29111", laravel = "#fb503b", botstrao = "#7952b3", node = "green";
 
 const MyProjects = [
     {/* PROJECT 1 */
@@ -341,6 +342,36 @@ const MyProjects = [
                 "img/project_11/1.png",
                 "img/project_11/2.png",
                 "img/project_11/3.png"
+            ]
+    },
+    {
+        en: {
+            projectTitle: "Full stack Todo List app",
+            projectInfo: "Organize tasks.",
+            projectBtnInfo: "More Information",
+            canvaBuiltwith: "Built with:"
+        },
+        es: {
+            projectTitle: "Full Stack Todo List app",
+            projectInfo: "Organizar tareas.",
+            projectBtnInfo: "Más Información",
+            canvaBuiltwith: "Construido con:"
+        },
+        imgStart: "img/project_12/1.png",
+        src: "https://splendid-macaron-d68a8f.netlify.app/",
+        tecnologies:
+            [
+                <FaReact size={60} color={reactJs} />,
+                <DiCss3 size={60} color={css} />,
+                <IoLogoJavascript size={60} color={js} />,
+                <FaNodeJs size={60} color={node} />,
+                <SiMysql size={60} color={mysql} />
+            ],
+        imgProject:
+            [
+                "img/project_12/1.png",
+                "img/project_12/2.png",
+                "img/project_12/3.png"
             ]
     },
 ]
